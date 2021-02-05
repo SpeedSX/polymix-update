9429b1b0fd00992ba03e0789a2b7df5c73c541f6
Oleksii R <arodionow@gmail.com>
2021-02-05 18:06:23 +0200

    refactored

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,63 @@
-use tiberius::{SqlBrowser, error::Error};
+use std::error::Error;
+use chrono::{DateTime, Utc};
+use tiberius::{Row, SqlBrowser};
 use tiberius::{Client, Config};
 use tokio::net::TcpStream;
 use tokio_util::compat::{Compat, TokioAsyncWriteCompatExt};
 
 pub type SqlConnection = Client<Compat<TcpStream>>;
 
-pub async fn connect(connection_string: &str) -> Result<SqlConnection, Error> {
-    let config = Config::from_ado_string(&connection_string)?;
+pub struct DB {
+    client: SqlConnection
+}
 
-    let tcp = TcpStream::connect_named(&config).await?;
-    tcp.set_nodelay(true)?;
+impl DB {
+    pub async fn connect(connection_string: &str) -> Result<DB, Box<dyn Error>> {
+        let config = Config::from_ado_string(&connection_string)?;
 
-    Client::connect(config.clone(), tcp.compat_write()).await
+        let tcp = TcpStream::connect_named(&config).await?;
+        tcp.set_nodelay(true)?;
+
+        let client = Client::connect(config.clone(), tcp.compat_write()).await?;
+
+        Ok(DB {
+            client
+        })
+    }
+    
+    pub async fn insert_file_name(&mut self, file_name: &str, file_date: DateTime<Utc>) -> Result<(), Box<dyn Error>> {
+        print!("Adding new file...");
+        self.client.execute("INSERT INTO PolyCalcVersion (FileName, FileDate) VALUES (@P1, @P2)", &[&file_name, &file_date]).await?;
+        println!("OK");
+        Ok(())
+    }
+
+    pub async fn upload_file_content(&mut self, file_name: &str, file_date: DateTime<Utc>, content: Vec<u8>) -> Result<(), Box<dyn Error>> {
+        print!("Uploading content...");
+        self.client.execute("UPDATE PolyCalcVersion set FileDate = @P1, FileImage = @P2 WHERE FileName = @P3", &[&file_date, &content, &file_name]).await?;
+        println!("OK");
+        Ok(())
+    }
+
+    pub async fn get_db_files(&mut self) -> Result<Vec<String>, Box<dyn Error>> {
+        let stream = self.client.query("select * from PolyCalcVersion", &[]).await?;
+
+        let result: Vec<String> = stream
+            .into_first_result()
+            .await?
+            .iter()
+            .map(|row| Self::try_get_string(&row, "FileName"))
+            .filter_map(|file_name| file_name)
+            .collect();
+
+        Ok(result)
+    }
+
+    fn try_get_string(row: &Row, col: &str) -> Option<String> {
+        // Error values are converted to empty strings
+        row.try_get::<&str, &str>(col)
+        .map(|value| value.map(|s| s.to_string()))
+        .unwrap_or_default()
+        //.map_or_else(|_| Some("".to_string()), |value| value.map(|s| s.to_string()))
+    }
 }
--- src/updater.rs
+++ src/updater.rs
@@ -1,9 +1,8 @@
 use std::{error::Error, fs, time::SystemTime};
 use chrono::{DateTime, Local, Utc};
 use glob::glob;
-use tiberius::{Row};
 
-use crate::{config::Config, db::{SqlConnection, connect}};
+use crate::{config::Config, db::DB};
 
 pub struct Updater<'a> {
     config: &'a Config,
@@ -31,9 +30,9 @@ impl Updater<'_> {
     }
 
     async fn upload_files(&self, file_mask: &str) -> Result<(), Box<dyn Error>> {
-        let mut client = self.get_connection().await?;
+        let mut client = DB::connect(&self.config.connection_string).await?;
 
-        let db_files = self.get_db_files(&mut client).await?;
+        let db_files = client.get_db_files().await?;
 
         for entry in glob(file_mask)? {
             let path = entry?;
@@ -56,57 +55,16 @@ impl Updater<'_> {
             let file_name: &String = &file_name.into();
             let file_date: DateTime<Utc> = last_modified.into();
             if !db_files.contains(file_name) {
-                self.insert_file_name(&mut client, file_name, file_date).await?;
+                client.insert_file_name(file_name, file_date).await?;
             }
-            self.upload_file_content(&mut client, file_name, file_date, content).await?;
+            client.upload_file_content(file_name, file_date, content).await?;
         }
 
-        Ok(())
-    }
-
-    async fn insert_file_name(&self, client: &mut SqlConnection, file_name: &str, file_date: DateTime<Utc>) -> Result<(), Box<dyn Error>> {
-        print!("Adding new file...");
-        client.execute("INSERT INTO PolyCalcVersion (FileName, FileDate) VALUES (@P1, @P2)", &[&file_name, &file_date]).await?;
-        println!("OK");
-        Ok(())
-    }
-
-    async fn upload_file_content(&self, client: &mut SqlConnection, file_name: &str, file_date: DateTime<Utc>, content: Vec<u8>) -> Result<(), Box<dyn Error>> {
-        print!("Uploading content...");
-        client.execute("UPDATE PolyCalcVersion set FileDate = @P1, FileImage = @P2 WHERE FileName = @P3", &[&file_date, &content, &file_name]).await?;
-        println!("OK");
         Ok(())
     }
 
     fn format_time(system_time: SystemTime) -> String {
         let datetime: DateTime<Local> = system_time.into();
         datetime.format("%d/%m/%Y %T").to_string()
-    }
-
-    async fn get_connection(&self) -> Result<SqlConnection, Box<dyn Error>> {
-        let connection = connect(&self.config.connection_string).await?;
-        Ok(connection)
-    }
-
-    async fn get_db_files(&self, client: &mut SqlConnection) -> Result<Vec<String>, Box<dyn Error>> {
-        let stream = client.query("select * from PolyCalcVersion", &[]).await?;
-
-        let result: Vec<String> = stream
-            .into_first_result()
-            .await?
-            .iter()
-            .map(|row| Self::try_get_string(&row, "FileName"))
-            .filter_map(|file_name| file_name)
-            .collect();
-
-        Ok(result)
-    }
-
-    fn try_get_string(row: &Row, col: &str) -> Option<String> {
-        // Error values are converted to empty strings
-        row.try_get::<&str, &str>(col)
-        .map(|value| value.map(|s| s.to_string()))
-        .unwrap_or_default()
-        //.map_or_else(|_| Some("".to_string()), |value| value.map(|s| s.to_string()))
     }
 }
