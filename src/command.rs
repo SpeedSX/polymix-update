af7f53ed8e57cc9e68c2810c9e0ae8f8fc1c074f
Oleksii Rodionov <orodionov@emergemarket.com>
2023-02-25 22:27:47 +0200

    refactored command parsing in order not to replicate command names

--- src/command.rs
+++ src/command.rs
@@ -1,21 +1,45 @@
 use std::str::FromStr;
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Display, EnumIter)]
 pub enum Command {
     Upload,
     Download,
     List,
 }
 
+impl Command {
+    fn from_str_case_insensitive(input: &str) -> Result<Command, ()> {
+        for variant in Command::iter() {
+            if input.eq_ignore_ascii_case(&variant.to_string()) {
+                return Ok(variant);
+            }
+        }
+        Err(())
+    }
+}
+
 impl FromStr for Command {
     type Err = ();
 
     fn from_str(input: &str) -> Result<Command, Self::Err> {
-        match input.to_lowercase().as_str() {
-            "upload" => Ok(Command::Upload),
-            "download" => Ok(Command::Download),
-            "list" => Ok(Command::List),
-            _ => Err(()),
-        }
+        Command::from_str_case_insensitive(input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_str_case_insensitive() {
+        assert_eq!(Command::from_str_case_insensitive("Upload"), Ok(Command::Upload));
+        assert_eq!(Command::from_str_case_insensitive("upload"), Ok(Command::Upload));
+        assert_eq!(Command::from_str_case_insensitive("Download"), Ok(Command::Download));
+        assert_eq!(Command::from_str_case_insensitive("download"), Ok(Command::Download));
+        assert_eq!(Command::from_str_case_insensitive("List"), Ok(Command::List));
+        assert_eq!(Command::from_str_case_insensitive("list"), Ok(Command::List));
+        assert_eq!(Command::from_str_case_insensitive("invalid"), Err(()));
     }
 }
